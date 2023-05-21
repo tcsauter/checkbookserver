a5693b885b145027c8959a575bc98870f2a8e475
Travis Sauter-Hunsberger <traviscsauter@gmail.com>
2023-05-21 18:13:40 -0400

    Initial commit

---
+++ src/main/java/com/noartist/checkbookserver/CheckbookserverApplication.java
@@ -0,0 +1,13 @@
+package com.noartist.checkbookserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckbookserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckbookserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/noartist/checkbookserver/CheckbookserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noartist.checkbookserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckbookserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
