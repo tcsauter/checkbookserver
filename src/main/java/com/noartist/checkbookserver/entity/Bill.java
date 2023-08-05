a39c9b9e65d0d315f5b004f41c4442083e861634
Travis Sauter-Hunsberger <traviscsauter@gmail.com>
2023-08-05 05:59:43 -0400

    Create Bill entity.

---
+++ src/main/java/com/noartist/checkbookserver/entity/Bill.java
@@ -0,0 +1,246 @@
+package com.noartist.checkbookserver.entity;
+
+import com.noartist.checkbookserver.exception.InvalidDueDateException;
+
+public class Bill implements Comparable<Bill> {
+    private String _id;
+    private String description;
+    private double amount;
+    private Frequency frequency;
+    private String due;
+    private boolean isPaidInInstallments;
+    private double paidSoFar;
+    private boolean isPaidFromBudget;
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getFrequency() {
+        switch(this.frequency) {
+            case Y -> {
+                return "Yearly";
+            }
+            case Q -> {
+                return "Quarterly";
+            }
+            case M -> {
+                return "Monthly";
+            }
+            default -> {
+                return "Bi-Weekly";
+            }
+        }
+    }
+
+    public void setFrequency(String frequency) {
+        switch(frequency.toLowerCase()) {
+            case "yearly" -> this.frequency = Frequency.Y;
+            case "quarterly" -> this.frequency = Frequency.Q;
+            case "monthly" -> this.frequency = Frequency.M;
+            case "bi-weekly" -> this.frequency = Frequency.B;
+        }
+    }
+
+    public String getDue() {
+        return due;
+    }
+
+    public void setDue(String due) throws InvalidDueDateException {
+        switch (this.frequency) {
+            case Y -> {
+                boolean isValidated = true;
+                if (due.contains("-")) {
+                    String[] dueSplit = due.split("-");
+                    if (dueSplit.length == 2) {
+                        try {
+                            int month = Integer.parseInt(dueSplit[0]);
+                            int day = Integer.parseInt(dueSplit[1]);
+                            if (month > 0 && month < 13) {
+                                switch (month) {
+                                    case 4, 6, 9, 11 -> {
+                                        if (day > 0 && day < 31) {
+                                            this.due = due;
+                                        } else {
+                                            isValidated = false;
+                                        }
+                                    }
+                                    case 2 -> {
+                                        if (day > 0 && day < 29) {
+                                            this.due = due;
+                                        } else {
+                                            isValidated = false;
+                                        }
+                                    }
+                                    default -> {
+                                        if (day > 0 && day < 32) {
+                                            this.due = due;
+                                        } else {
+                                            isValidated = false;
+                                        }
+                                    }
+                                }
+                            } else {
+                                isValidated = false;
+                            }
+                        } catch (NumberFormatException e) {
+                            isValidated = false;
+                        }
+                    } else {
+                        isValidated = false;
+                    }
+                } else {
+                    isValidated = false;
+                }
+
+                if(!isValidated) {
+                    throw new InvalidDueDateException("Due Date must be in mm-dd format for Yearly bills.");
+                }
+            }
+            case M -> {
+                boolean isValidated = true;
+                try {
+                    int day = Integer.parseInt(due);
+                    if(day > 0 && day < 32) {
+                        this.due = due;
+                    } else {
+                        isValidated = false;
+                    }
+                } catch(NumberFormatException e) {
+                    isValidated = false;
+                }
+
+                if(!isValidated) {
+                    throw new InvalidDueDateException("Due Date must be a valid day of the month for Monthly bills.");
+                }
+            }
+            case B -> due = "Bi-Weekly";
+            case Q -> due = "Quarterly";
+        }
+    }
+
+    public boolean isPaidInInstallments() {
+        return isPaidInInstallments;
+    }
+
+    public void setPaidInInstallments(boolean paidInInstallments) {
+        isPaidInInstallments = paidInInstallments;
+    }
+
+    public double getPaidSoFar() {
+        return paidSoFar;
+    }
+
+    public void setPaidSoFar(double paidSoFar) {
+        this.paidSoFar = paidSoFar;
+    }
+
+    public boolean isPaidFromBudget() {
+        return isPaidFromBudget;
+    }
+
+    public void setPaidFromBudget(boolean paidFromBudget) {
+        isPaidFromBudget = paidFromBudget;
+    }
+
+    @Override
+    public int compareTo(Bill o) {
+        //Ordering of bills is: By Frequency
+        //                      By Due Date
+        //                      By Description
+        if(this.frequency.equals(o.frequency)) {
+            if(this.due.equals(o.due)) {
+                if(this.description.equals(o.description)) {
+                    return 0;
+                } else {
+                    return this.description.compareTo(o.description);
+                }
+            } else {
+                return this.due.compareTo(o.due);
+            }
+        } else {
+            switch(this.frequency) {
+                case Y -> {
+                    return 1;
+                }
+                case Q -> {
+                    if(o.frequency.equals(Frequency.Y)) {
+                        return -1;
+                    } else {
+                        return 1;
+                    }
+                }
+                case M -> {
+                    if(o.frequency.equals(Frequency.Y) || o.frequency.equals(Frequency.Q)) {
+                        return -1;
+                    } else {
+                        return 1;
+                    }
+                }
+                default -> {
+                    return -1;
+                }
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Bill{" +
+                "_id='" + _id + '\'' +
+                ", description='" + description + '\'' +
+                ", amount=" + amount +
+                ", frequency=" + frequency +
+                ", due='" + due + '\'' +
+                ", isPaidInInstallments=" + isPaidInInstallments +
+                ", paidSoFar=" + paidSoFar +
+                ", isPaidFromBudget=" + isPaidFromBudget +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Bill) {
+            return this.compareTo((Bill) obj) == 0;
+        } else {
+            return false;
+        }
+    }
+
+    private enum Frequency {
+        B("Bi-weekly"),
+        M("Monthly"),
+        Q("Quarterly"),
+        Y("Yearly");
+
+        private final String frequencyString;
+
+        Frequency(String freqString) {
+            this.frequencyString = freqString;
+        }
+        @Override
+        public String toString() {
+            return frequencyString;
+        }
+    }
+}
---
+++ src/main/java/com/noartist/checkbookserver/exception/InvalidDueDateException.java
@@ -0,0 +1,7 @@
+package com.noartist.checkbookserver.exception;
+
+public class InvalidDueDateException extends Exception{
+    public InvalidDueDateException(String message) {
+        super(message);
+    }
+}
